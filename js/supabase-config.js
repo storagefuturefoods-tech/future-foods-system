65cf69bcbf8dbabf57fffefa08ec83309e24a90b
storagefuturefoods-tech <storage.futurefoods@gmail.com>
2026-08-12 15:20:02 +0300

    Update supabase-config.js

--- js/supabase-config.js
+++ js/supabase-config.js
@@ -1,1 +1,5 @@
+// استدعاء مكتبة Supabase عبر CDN
+const SUPABASE_URL = "YOUR_SUPABASE_URL_HERE";
+const SUPABASE_KEY = "YOUR_SUPABASE_ANON_KEY_HERE";
 
+const _supabase = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
